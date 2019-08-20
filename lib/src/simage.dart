af8eb83d4ff4037d13e8d750efa65336cd4b6efd
Samiratu  Ntohsi <samiratuntohsi@gmail.com>
2019-08-20 18:06:39 +0200

    updating stylist details is complete

--- lib/src/simage.dart
+++ lib/src/simage.dart
@@ -3,28 +3,34 @@ import './stylist.dart';
 
 class StylistImage extends StatelessWidget {
   final String imageUrl;
-  const StylistImage(
-      {Key key,
-        this.imageUrl})
-      : super(key: key);
+  const StylistImage({Key key, this.imageUrl}) : super(key: key);
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.purple,
-        leading: IconButton(icon: Icon(Icons.arrow_back, color: Colors.white,), onPressed: (){
-          Navigator.pushReplacement(
-            context,
-            MaterialPageRoute(builder: (context) => StylistPage()),
-          );
-        }),
+        leading: IconButton(
+            icon: Icon(
+              Icons.arrow_back,
+              color: Colors.white,
+            ),
+            onPressed: () {
+              Navigator.pushReplacement(
+                context,
+                MaterialPageRoute(builder: (context) => StylistPage()),
+              );
+            }),
       ),
       body: Container(
         color: Colors.white,
-//        margin:EdgeInsets.all(30.0),
-        child: Image.network("$imageUrl", fit: BoxFit.cover,),
+        width: 300.0,
+        height: 450.0,
+        margin: EdgeInsets.all(10.0),
+        child: Image.network(
+          "$imageUrl",
+          fit: BoxFit.cover,
+        ),
       ),
     );
-
   }
 }
